8e5f3f56f04be86491b1586b5984f3872b369fa6
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-02 21:38:30 +0100

    Adding initial ReceiveMessageResponseReader and tests

---
+++ src/Aws.Sqs.Core/SqsResponseConstants.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+
+namespace HighPerfCloud.Aws.Sqs.Core
+{
+    internal static class SqsResponseConstants
+    {
+        public static ReadOnlySpan<byte> MessageTagStart => new byte[] { (byte)'<', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
+        public static ReadOnlySpan<byte> MessageTagEnd => new byte[] { (byte)'<', (byte)'/', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
+    }
+}
