57e51d5a16f569372ef3241bc42b4f3e99268c27
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-03 08:27:55 +0100

    Attempt  to optimise CountMessages
    This proved to be slower in benchmark tests. Including in this commit for reference.
    
    The slower v2 version will be removed in the next commit.

--- src/Aws.Sqs.Core/ReceiveMessageResponseReader.cs
+++ src/Aws.Sqs.Core/ReceiveMessageResponseReader.cs
@@ -20,10 +20,48 @@ namespace HighPerfCloud.Aws.Sqs.Core
             var remainingData = bytes;
             int index;
 
-            while(count < MaxMessages && (index = remainingData.IndexOf(SqsResponseConstants.MessageTagStart)) != -1) 
-            {               
+            while (count < MaxMessages && (index = remainingData.IndexOf(SqsResponseConstants.MessageTagStart)) != -1)
+            {
                 remainingData = remainingData.Slice(index + SqsResponseConstants.MessageTagStart.Length);
                 count++;
+            }
+
+            return count;
+        }
+
+        [Obsolete("This version benchmarked slower than the original")]
+        internal int CountMessagesV2(in ReadOnlySpan<byte> bytes)
+        {
+            if (bytes.Length == 0)
+                return 0;
+            
+            var count = 0;
+            var remainingData = bytes;
+            int openAnglePosition;
+
+            while (count < MaxMessages && (openAnglePosition = remainingData.IndexOf(SqsResponseConstants.OpenAngleBracket)) != -1)
+            {
+                var closeAnglePosition = remainingData.IndexOf(SqsResponseConstants.CloseAngleBracket);
+
+                if (closeAnglePosition == -1)
+                    break; // invalid XML
+
+                if (remainingData[openAnglePosition + 1] != SqsResponseConstants.ForwardSlash) // not a closing tag '</'
+                {
+                    var lengthOfTag = closeAnglePosition - openAnglePosition - 1;
+
+                    if (lengthOfTag == SqsResponseConstants.MessageTag.Length)
+                    {
+                        var tag = remainingData.Slice(openAnglePosition + 1, lengthOfTag);
+
+                        if (tag.SequenceEqual(SqsResponseConstants.MessageTag))
+                        { 
+                            count++;
+                        }
+                    }
+                }
+
+                remainingData = remainingData.Slice(closeAnglePosition + 1);
             }
 
             return count;
--- src/Aws.Sqs.Core/SqsResponseConstants.cs
+++ src/Aws.Sqs.Core/SqsResponseConstants.cs
@@ -5,7 +5,13 @@ namespace HighPerfCloud.Aws.Sqs.Core
 {
     internal static class SqsResponseConstants
     {
-        public static ReadOnlySpan<byte> MessageTagStart => new byte[] { (byte)'<', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
-        public static ReadOnlySpan<byte> MessageTagEnd => new byte[] { (byte)'<', (byte)'/', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
+        public static byte OpenAngleBracket = (byte)'<';
+        public static byte CloseAngleBracket = (byte)'>';
+        public static byte ForwardSlash = (byte)'/';
+
+        public static ReadOnlySpan<byte> MessageTag => new[] { (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e' };
+
+        public static ReadOnlySpan<byte> MessageTagStart => new[] { (byte)'<', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
+        public static ReadOnlySpan<byte> MessageTagEnd => new[] { (byte)'<', (byte)'/', (byte)'M', (byte)'e', (byte)'s', (byte)'s', (byte)'a', (byte)'g', (byte)'e', (byte)'>' };
     }
 }
