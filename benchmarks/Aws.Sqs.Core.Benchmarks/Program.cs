3285a4605069e0b7d93013ddb7c4c413f8782264
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-03 07:44:33 +0100

    Minor code cleanup and renaming

--- benchmarks/Aws.Sqs.Core.Benchmarks/Program.cs
+++ benchmarks/Aws.Sqs.Core.Benchmarks/Program.cs
@@ -7,24 +7,24 @@ namespace Aws.Sqs.Core.Benchmarks
 {
     class Program
     {
-        static void Main(string[] args) => _ = BenchmarkRunner.Run<ReceiveMessageResponseReader_CountMessages>();
+        static void Main(string[] args) => _ = BenchmarkRunner.Run<ReceiveMessageResponseReaderCountMessages>();
     }
 
     [MemoryDiagnoser]
-    public class ReceiveMessageResponseReader_CountMessages
+    public class ReceiveMessageResponseReaderCountMessages
     {
         private byte[] _responseBytes;
 
         [GlobalSetup]
         public void Setup()
         {            
-            var responseStart = @"<ReceiveMessageResponse><ReceiveMessageResult>";
-            var responseEnd = @"</ReceiveMessageResult><ResponseMetadata><RequestId>b6633655-283d-45b4-aee4-4e84e0ae6afa</RequestId></ResponseMetadata></ReceiveMessageResponse>";
-            var message = @"<Message><MessageId>5fea7756-0ea4-451a-a703-a558b933e274</MessageId><ReceiptHandle>MbZj6wDWli=</ReceiptHandle><MD5OfBody>fafb00f5732ab283681e124bf8747ed1</MD5OfBody><Body>This is a test message</Body><Attribute><Name>SentTimestamp</Name><Value>1238099229000</Value></Attribute></Message>";
+            const string responseStart = @"<ReceiveMessageResponse><ReceiveMessageResult>";
+            const string responseEnd = @"</ReceiveMessageResult><ResponseMetadata><RequestId>b6633655-283d-45b4-aee4-4e84e0ae6afa</RequestId></ResponseMetadata></ReceiveMessageResponse>";
+            const string message = @"<Message><MessageId>5fea7756-0ea4-451a-a703-a558b933e274</MessageId><ReceiptHandle>MbZj6wDWli=</ReceiptHandle><MD5OfBody>fafb00f5732ab283681e124bf8747ed1</MD5OfBody><Body>This is a test message</Body><Attribute><Name>SentTimestamp</Name><Value>1238099229000</Value></Attribute></Message>";
 
             var sb = new StringBuilder();
             sb.Append(responseStart);
-            for (int i = 0; i <= Count; i++)
+            for (var i = 0; i <= Count; i++)
             {
                 sb.Append(message);
             }
@@ -33,7 +33,7 @@ namespace Aws.Sqs.Core.Benchmarks
             _responseBytes = Encoding.UTF8.GetBytes(sb.ToString());
         }
 
-        [Params(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)]
+        [Params(0, 5, 10)]
         public int Count;
 
         [Benchmark]
