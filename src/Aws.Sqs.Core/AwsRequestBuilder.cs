6b432718d249dc7913cdc61e8dda2ed04513c47e
Steve Gordon <sgordon@hotmail.co.uk>
2019-05-07 21:16:52 +0100

    Adding initial SqsMessageMemoryPool concept

---
+++ src/Aws.Sqs.Core/AwsRequestBuilder.cs
@@ -0,0 +1,56 @@
+﻿
+//using HighPerfCloud.Aws.Sqs.Core.Primitives;
+//using System;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+
+//namespace HighPerfCloud.Aws.Sqs.Core
+//{
+//    internal struct AwsRequestBuilder
+//    {
+
+
+//        public HttpRequestMessage BuildReceiveMessage(in AwsRegion region, AccountId accountId, string queueName)
+//        {
+
+//        }
+
+
+//        //public static DateTime InitializeHeaders(HttpRequestHeaders headers, Uri requestEndpoint, DateTime requestDateTime)
+//        //{
+   
+
+//        //    if (!headers.ContainsKey(HeaderKeys.HostHeader))
+//        //    {
+//        //        var hostHeader = requestEndpoint.Host;
+//        //        if (!requestEndpoint.IsDefaultPort)
+//        //            hostHeader += ":" + requestEndpoint.Port;
+//        //        headers.Add(HeaderKeys.HostHeader, hostHeader);
+//        //    }
+
+//        //    var dt = requestDateTime;
+//        //    headers[HeaderKeys.XAmzDateHeader] = dt.ToUniversalTime().ToString(AWSSDKUtils.ISO8601BasicDateTimeFormat, CultureInfo.InvariantCulture);
+
+//        //    return dt;
+//        //}
+//    }
+
+//    public readonly struct SqsQueueReader
+//    {
+//        private readonly string _region;
+//        private readonly string _accountNumber;
+//        private readonly string _queueName;
+
+//        public SqsQueueReader(in AwsRegion region, AccountId accountId, string queueName)
+//        {
+//            _region = region;
+//            _accountNumber = accountNumber;
+//            _queueName = queueName;
+//        }
+
+//        public void ReceieveSingleMessage()
+//        {
+
+//        }
+//    }
+//}
---
+++ src/Aws.Sqs.Core/SqsMessageMemory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Buffers;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HighPerfCloud.Aws.Sqs.Core
+{
+    internal static class SqsMessageMemoryPool
+    {
+        internal static ValueTask<IMemoryOwner<byte>> RentAndPopulateFromStreamAsync(Stream stream, int contentLength)
+        {
+            var buffer = ArrayPool<byte>.Shared.Rent(contentLength);
+
+            var readTask = stream.ReadAsync(buffer);
+
+            if (readTask.IsCompletedSuccessfully)
+                return new ValueTask<IMemoryOwner<byte>>(new SqsMessageMemoryOwner(buffer, contentLength));
+
+            return AwaitAndReturnAsync(readTask, buffer);
+
+            async ValueTask<IMemoryOwner<byte>> AwaitAndReturnAsync(ValueTask<int> runningTask, byte[] localBuffer)
+            {
+                await runningTask.ConfigureAwait(false);
+
+                return new SqsMessageMemoryOwner(localBuffer, contentLength);
+            }
+        }
+
+        private struct SqsMessageMemoryOwner : IMemoryOwner<byte>
+        {
+            private readonly int _length;
+            private byte[]? _oversized;
+
+            internal SqsMessageMemoryOwner(byte[] oversized, int length)
+            {
+                _length = length;
+                _oversized = oversized;
+            }
+
+            public Memory<byte> Memory => new Memory<byte>(GetArray(), 0, _length);
+
+            private byte[] GetArray() =>
+                Interlocked.CompareExchange(ref _oversized, null, null) ?? throw new ObjectDisposedException(ToString());
+
+            public void Dispose()
+            {
+                var arr = Interlocked.Exchange(ref _oversized, null);
+                if (arr != null) ArrayPool<byte>.Shared.Return(arr);
+            }
+        }
+    }
+}
